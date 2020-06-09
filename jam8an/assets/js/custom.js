abadf815f317d3bd776969c49e6a9700df48ef4b
breakdesign <breakdesign28@gmail.com>
2020-06-09 22:47:26 +0700

    dashboard v.1

--- jam8an/assets/js/custom.js
+++ jam8an/assets/js/custom.js
@@ -132,80 +132,149 @@ var custom = function () {
     }, error, options);
   }
 
-  var leaflet_map_only_suggestion = function () {
-
-    // enableHighAccuracy to get high accuracy location (working on mobile device)
-    var options = {
-      enableHighAccuracy: true,
-      timeout: 5000,
-    };
-
-    function error(err) {
-      console.warn(`ERROR(${err.code}): ${err.message}`);
-    }
-
-    // navigator for get current position
-    navigator.geolocation.getCurrentPosition(function (location) {
-      var latlng = new L.LatLng(location.coords.latitude, location.coords.longitude);
-
-      // set view current position
-      var map = L.map('map').setView(latlng, 15)
-
-      L.tileLayer('http://{s}.google.com/vt/lyrs=m&x={x}&y={y}&z={z}', {
-        maxZoom: 20,
-        subdomains:['mt0','mt1','mt2','mt3'],
-      }).addTo(map);
-
-      var geocodeService = L.esri.Geocoding.geocodeService();
-      var searchControl = L.esri.Geocoding.geosearch().addTo(map);
-
-      // set data map in textbox
-      document.getElementById('latitude').value = latlng.lat;
-      document.getElementById('longitude').value = latlng.lng;
-      document.getElementById('addressInput').value = latlng.address;
-
-      // set marker on current location
-      var marker = L.marker(latlng).addTo(map);
-
-      // create new layer group for save search result
-      var results = new L.LayerGroup().addTo(map);
-      
-      // event click for get data on map
-      map.on('click', function (e) {
-        // geocodeService.reverse() => to get data location as address latitude and longtitude
-        geocodeService.reverse().latlng(e.latlng).run(function (error, result) {
-          if (error) {
-            return;
-          }
-
-          // clear marker on map before create marker
-          if (marker) {
-            results.clearLayers();
-            map.removeLayer(marker);
-          }
-
-          // to add marker and popup result address
-          marker = L.marker(result.latlng).addTo(map).bindPopup(result.address.Match_addr).openPopup();
-
-          document.getElementById('latitude').value = result.latlng.lat;
-          document.getElementById('longitude').value = result.latlng.lng;
-          document.getElementById('addressInput').value = result.address.Match_addr;
-        });
-      });
-
-      // event get result from search
-      searchControl.on('results', function (data) {
-        results.clearLayers();
-        for (var i = data.results.length - 1; i >= 0; i--) {
-          results.addLayer(L.marker(data.results[i].latlng));
-          document.getElementById('latitude').value = data.results[i].latlng.lat;
-          document.getElementById('longitude').value = data.results[i].latlng.lng;
-          document.getElementById('addressInput').value = data.results[i].properties.Match_addr;
-        }
-      });
-
-    }, error, options);
+  var chart_user_upgrader = function () {
+    new Chart('jsChartUserUpgrader', {
+			type: 'line',
+			data: {
+				labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+				datasets: [{
+					label: 'User',
+					backgroundColor: 'rgb(54, 162, 235)',
+					borderColor: 'rgb(54, 162, 235)',
+					data: [
+						10,
+						12,
+						15,
+						22,
+						27,
+						30,
+						40
+					],
+					fill: false,
+				}, {
+					label: 'Upgrader',
+					fill: false,
+					backgroundColor: 'rgb(76, 175, 80)',
+					borderColor: 'rgb(76, 175, 80)',
+					data: [
+						8,
+						10,
+						12,
+						15,
+						27,
+						28,
+						30
+					],
+				}]
+			},
+			options: {
+				responsive: true,
+				title: {
+					display: false,
+					text: 'Users vs Upgrader'
+				},
+				tooltips: {
+					mode: 'index',
+					intersect: false,
+				},
+				hover: {
+					mode: 'nearest',
+					intersect: true
+				},
+				scales: {
+					xAxes: [{
+						display: true,
+						scaleLabel: {
+							display: true,
+							labelString: 'Month'
+						}
+					}],
+					yAxes: [{
+						display: true,
+						scaleLabel: {
+							display: true,
+							labelString: 'Value'
+						}
+					}]
+				}
+			}
+		})
   }
+
+  // var leaflet_map_only_suggestion = function () {
+
+  //   // enableHighAccuracy to get high accuracy location (working on mobile device)
+  //   var options = {
+  //     enableHighAccuracy: true,
+  //     timeout: 5000,
+  //   };
+
+  //   function error(err) {
+  //     console.warn(`ERROR(${err.code}): ${err.message}`);
+  //   }
+
+  //   // navigator for get current position
+  //   navigator.geolocation.getCurrentPosition(function (location) {
+  //     var latlng = new L.LatLng(location.coords.latitude, location.coords.longitude);
+
+  //     // set view current position
+  //     var map = L.map('map').setView(latlng, 15)
+
+  //     L.tileLayer('http://{s}.google.com/vt/lyrs=m&x={x}&y={y}&z={z}', {
+  //       maxZoom: 20,
+  //       subdomains:['mt0','mt1','mt2','mt3'],
+  //     }).addTo(map);
+
+  //     var geocodeService = L.esri.Geocoding.geocodeService();
+  //     var searchControl = L.esri.Geocoding.geosearch().addTo(map);
+
+  //     // set data map in textbox
+  //     document.getElementById('latitude').value = latlng.lat;
+  //     document.getElementById('longitude').value = latlng.lng;
+  //     document.getElementById('addressInput').value = latlng.address;
+
+  //     // set marker on current location
+  //     var marker = L.marker(latlng).addTo(map);
+
+  //     // create new layer group for save search result
+  //     var results = new L.LayerGroup().addTo(map);
+      
+  //     // event click for get data on map
+  //     map.on('click', function (e) {
+  //       // geocodeService.reverse() => to get data location as address latitude and longtitude
+  //       geocodeService.reverse().latlng(e.latlng).run(function (error, result) {
+  //         if (error) {
+  //           return;
+  //         }
+
+  //         // clear marker on map before create marker
+  //         if (marker) {
+  //           results.clearLayers();
+  //           map.removeLayer(marker);
+  //         }
+
+  //         // to add marker and popup result address
+  //         marker = L.marker(result.latlng).addTo(map).bindPopup(result.address.Match_addr).openPopup();
+
+  //         document.getElementById('latitude').value = result.latlng.lat;
+  //         document.getElementById('longitude').value = result.latlng.lng;
+  //         document.getElementById('addressInput').value = result.address.Match_addr;
+  //       });
+  //     });
+
+  //     // event get result from search
+  //     searchControl.on('results', function (data) {
+  //       results.clearLayers();
+  //       for (var i = data.results.length - 1; i >= 0; i--) {
+  //         results.addLayer(L.marker(data.results[i].latlng));
+  //         document.getElementById('latitude').value = data.results[i].latlng.lat;
+  //         document.getElementById('longitude').value = data.results[i].latlng.lng;
+  //         document.getElementById('addressInput').value = data.results[i].properties.Match_addr;
+  //       }
+  //     });
+
+  //   }, error, options);
+  // }
 
   return {
     init: function () {
@@ -213,6 +282,8 @@ var custom = function () {
       select2();
       select2_assigne();
       leaflet_map();
+      chart_user_upgrader();
+      // leaflet_map_dashboard();
     }
   }
 }();
